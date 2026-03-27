d9d07dd0a11612dfc5475e9d49335c9278a78a32
sergio <@658054>
2026-03-26 21:55:24 -0600

    fix: Finalize routes after merge

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,6 @@
 
 use App\Http\Controllers\CatalogoController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CatalogoController;
 
-<<<<<<< HEAD
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', [CatalogoController::class, 'index'])->name('welcome');
 Route::get('/catalogo', [CatalogoController::class, 'index'])->name('catalogo.index');
-=======
-Route::get('/', [CatalogoController::class, 'index']);
->>>>>>> 90cd85b2ed5c307254edf69931f1cf84e6b42b8f
